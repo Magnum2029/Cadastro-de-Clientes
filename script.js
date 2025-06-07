ffe9084347bc31a7a438696646651b826c31d85b
Magnum Santos <magnumsantos97@gmail.com>
2025-06-07 14:02:37 +0100

    atualizacao

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiBaseUrl = "https://crudcrud.com/api/33c66541604049e1b6da7277c7b6167d/clientes";
+const apiBaseUrl = "https://crudcrud.com/api/81ee346ab2b2408d8d45444a3067b743/unicórnios";
 
 const form = document.getElementById("cliente-form");
 const nomeInput = Document.getElementById("nome");
@@ -6,24 +6,28 @@ const emailInput = document.getElementById("email");
 const listaClientes = document.getElementById("clientes-lista");
 
 // Carregar clientes ao iniciar
-window.addEventListener("DOMContentLoaded", carregarClientes);
+document.addEventListener("DOMContentLoaded", () => {
+    carregarClientes();
+});
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     const cliente = {
-        nome: nomeInput.value,
-        email: emailInput.value
+        nome: nomeInput.value.trim(),
+        email: emailInput.value.trim()
     };
+
+    if (!cliente.nome || !cliente.email) return;
 
     fetch(apiBaseUrl, {
         method: "POST",
         headers: {"Content-Type": "application/jon" },
         body: JSON.stringify(cliente)
     })
-    .then(response => response.json() )
+    .then(res => res.json() )
     .then(data => {
-        adicionarClienteNaLista(data);
+        adicionarClienteNaTela(data);
         form.reset();
     })
     .catch(err => console.error("Erro ao cadastrar:", err));
@@ -31,33 +35,29 @@ form.addEventListener("submit", function (e) {
 
 function carregarClientes() {
     fetch(apiBaseUrl)
-    .then(response => response.json())
+    .then(res => res.json())
     .then(clientes => {
         listaClientes.innerHTML = "";
-        clientes.forEach(adicionarClienteNaLista);
+        clientes.forEach(adicionarClienteNaTela);
     })
-    .catch(err => console.error("Erro ao carregar clientes:",));
+    .catch(err => console.error("Erro ao carregar:", err));
 }
 
-function adicionarClienteNaLista(cliente) {
+function adicionarClienteNaTela(cliente) {
     const li = document.createElement("li");
-    li.textContent = `${cliente.nome} - ${cliente.email}`;
+    li.innerHTML = `
+    <span><strong>${cliente.nome}</strong> - ${cliente.email}</span>
+    <button class="delete-btn">Excluir</button>
+    `;
 
-    const btn = document.createElement("button");
-    btn.textContent = "Excluir";
-    btn.className = "delete-btn";
-    btn.onclick = () => deletarCliente(cliente._id, li);
+    const btnExcluir = li.querySelector(".delete-btn");
+    btnExcluir.addEventListener("click", () => {
+     fetch(`${apiBaseUrl}/${cliente._id}`, {
+        method:"DELETE"
+     })
+     .then(() => li.remove())
+     .catch(err => console.error("erro ao excluir cliente:", err));
+    });
 
-    li.appendChild(btn);
     listaClientes.appendChild(li);
-}
-
-function deletarCliente(id, liElement) {
-    fetch(`${apiBaseUrl}/${id}`, {
-        method: "DELETE"
-    })
-    .then(() => {
-        liElement.remove();
-    })
-    .catch(err => console.error("Erro ao excluir cliente:", err));
 }
